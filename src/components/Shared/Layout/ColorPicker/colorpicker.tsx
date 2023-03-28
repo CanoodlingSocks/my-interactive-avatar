add049a0fa08305a34360b0d9c8bb37856f8e97d
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-03-28 09:40:07 +0200

    polish colourpicker

--- src/components/Shared/Layout/ColorPicker/colorpicker.tsx
+++ src/components/Shared/Layout/ColorPicker/colorpicker.tsx
@@ -3,6 +3,8 @@ import { HexColorPicker } from "react-colorful";
 import { Drawer, Button, Box } from "@mui/material";
 import PaletteIcon from '@mui/icons-material/Palette';
 import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
 
 interface ColorPickerProps {
     drawerOpen: boolean;
@@ -41,7 +43,12 @@ export const ColorPicker = ({ drawerOpen, setDrawerOpen }: ColorPickerProps) => 
       className
     ) as HTMLCollectionOf<SVGElement>;
     for (let i = 0; i < elements.length; i++) {
-      elements[i].style.fill = newColor;
+      const element = elements[i];
+      if (!element.hasAttribute("data-color")) {
+        // Store the original color as a data attribute
+        element.setAttribute("data-color", element.style.fill);
+      }
+      element.style.fill = newColor;
     }
   }
 
@@ -53,22 +60,63 @@ export const ColorPicker = ({ drawerOpen, setDrawerOpen }: ColorPickerProps) => 
     changeColor("shirt", color);
   };
 
-  const resetColors = () => {
+  const handleClickLips = () => {
+    changeColor("lips", color);
+  };
+
+  const handleClickEyeLids = () => {
+    changeColor("eyeLids", color);
+  };
+
+  const handleClickGlasses = () => {
+    changeColor("glasses", color);
+  };
+
+  const handleClickHearingAids = () => {
+    changeColor("hearingaid-inner", color);
+  };
+
+  function resetColors() {
+    const elements = document.querySelectorAll("[data-color]");
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i] as SVGElement;
+      const originalColor = element.getAttribute("data-color");
+      if (originalColor !== null) {
+        element.style.fill = originalColor ?? "#000000";
+      }
+    }
     setColor("#000000");
-    changeColor("hair", "#000000");
-    changeColor("shirt", "#000000");
   }
 
   return (
     <>
       <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)} anchor="left">
-        <Box width="300px" padding="1rem">
-          <HexColorPicker color={color} onChange={handleColorChange} />
+        <Box width="300px" padding="1rem" textAlign="center">
+          <Box display="flex" justifyContent="center">
+            <HexColorPicker color={color} onChange={handleColorChange} />
+          </Box>
+          <Typography variant="h6" gutterBottom>
+            Hair
+          </Typography>
           <Button onClick={handleClickHair}>Change Hair Color</Button>
+          <Typography variant="h6" gutterBottom>
+            Makeup
+          </Typography>
+          <Button onClick={handleClickLips}>Lipstick</Button>
+          <Button onClick={handleClickEyeLids}>Eyeshadow</Button>
+          <Typography variant="h6" gutterBottom>
+            Clothing & Accessories
+          </Typography>
+          <Button onClick={handleClickGlasses}>Change Glasses Color</Button>
           <Button onClick={handleClickShirt}>Change Shirt Color</Button>
-          <Button onClick={resetColors}>Reset</Button>
+          <Button onClick={handleClickHearingAids}>Change Hearingaids Color</Button>
+        </Box>
+        <Box textAlign="center">
+          <Button variant="contained" onClick={resetColors}>
+            Reset
+          </Button>
         </Box>
       </Drawer>
     </>
-  );
+  );  
 };
